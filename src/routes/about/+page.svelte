007adebeeb5f8ebb25169c042ce40b5519f4dfd7
Yuil <yuil.trip@mail.kmutt.ac.th>
2024-12-18 06:15:09 +0700

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,4 +13,12 @@
 <div class="bg-baby_powder text-blackie p-2">
 	<h1 class="text-3xl font-bold">Our Heritage</h1>
 	<p class="font-sans">Coming from the flashback.</p>
+	Check <a 
+	  href="https://www.jsae.or.jp/files_publish/page/827/no_29.pdf" 
+	  target="_blank" 
+	  rel="noopener noreferrer"
+	  class="text-blue-600 hover:underline"
+	>
+	  FSAE Japan 2014 report
+	</a> here.
 </div>

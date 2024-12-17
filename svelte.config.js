c0b608488dd6f03627d4f778653305577a7b0bc1
Yuil Tripathee <yuil.trip@mail.kmutt.ac.th>
2024-12-18 04:23:46 +0700

    Fix: rendering issues on web with URLs

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,22 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      fallback: '404.html',
-      pages: 'build',
-      assets: 'build',
-      precompress: false,
-      strict: true
+      // fallback: '404.html',
+      // pages: 'build',
+      // assets: 'build',
+      // precompress: false,
+      // strict: true
     }),
-    paths: {
-      // base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    // paths: {
+    //   // base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    //   base: '',
+    // },
+    router: {
       base: '',
+      trailingSlash: 'always'
+    },
+    prerender: {
+      default: true
     }
   },
 

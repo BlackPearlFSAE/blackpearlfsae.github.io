4b4829dd92ff857b8cb543a7c6a4488ca79267a6
Yuil Tripathee <yuil.trip@mail.kmutt.ac.th>
2024-12-18 05:24:33 +0700

    Fix: footer layout

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,7 @@
 <!-- src/lib/Footer.svelte -->
 <footer class="bg-baby_powder border-t-amber_SAE_ECE text-blackie border-t-4 py-4">
 	<div class="container mx-auto mt-auto text-center">
-		<p>&copy; {new Date().getFullYear()} My Website. <br /> Black Pearl Racing Team @ KMUTT</p>
+		<p>&copy; {new Date().getFullYear()} Black Pearl Racing Team <br /> @ KMUTT</p>
 		<p>
 			<a href="/" class="text-blue-700 hover:underline">Home</a> |
 			<a href="/about" class="text-blue-700 hover:underline">About</a>

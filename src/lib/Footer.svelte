47cc7c96c2542c9944021289596aed561d12d57b
Yuil Tripathee <yuil.trip@mail.kmutt.ac.th>
2024-12-19 21:18:11 +0700

    Add: Social handles on Footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,10 +1,15 @@
 <!-- src/lib/Footer.svelte -->
-<footer class="bg-baby_powder border-t-amber_SAE_ECE text-blackie border-t-4 py-4">
+<footer class="border-t-4 border-t-amber_SAE_ECE bg-baby_powder py-4 text-blackie">
 	<div class="container mx-auto mt-auto text-center">
 		<p>&copy; {new Date().getFullYear()} Black Pearl Racing Team <br /> @ KMUTT</p>
 		<p>
 			<a href="/" class="text-blue-700 hover:underline">Home</a> |
-			<a href="/about" class="text-blue-700 hover:underline">About</a>
+			<a href="/about" class="text-blue-700 hover:underline">About</a> |
+			<a href="https://github.com/BlackPearlFSAE" class="text-blue-700 hover:underline">GitHub</a> |
+			<a
+				href="https://www.instagram.com/blackpearlracingteam/"
+				class="text-blue-700 hover:underline">Instagram</a
+			>
 		</p>
 	</div>
 </footer>
